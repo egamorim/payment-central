2db8b2fd9a7414d5382439424df909a709207ecc
Alex sandro <alexcomput@gmail.com>
2021-04-13 12:27:37 -0300

    refact: Sql properties

--- src/main/java/br/com/zup/paymentcentral/config/SQSProperties.java
+++ src/main/java/br/com/zup/paymentcentral/config/SQSProperties.java
@@ -3,20 +3,35 @@ package br.com.zup.paymentcentral.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
+
+@Validated
 @Setter
 @Getter
 @ConfigurationProperties("payments.sqs")
 public class SQSProperties {
 
+    @NotNull
     private String url;
+
+    @NotNull
     private String accesskey;
+
+    @NotNull
     private String secretkey;
+
+    @NotNull
+    private String awsSqs;
+
     private SQSQueueProperties queue;
 
     @Getter
     @Setter
     public static class SQSQueueProperties{
+
+        @NotNull
         private String ted;
     }
 }
--- src/main/java/br/com/zup/paymentcentral/integration/PaymentsRoutes.java
+++ src/main/java/br/com/zup/paymentcentral/integration/PaymentsRoutes.java
@@ -4,63 +4,33 @@ import br.com.zup.paymentcentral.config.KafkaProperties;
 import br.com.zup.paymentcentral.config.SQSProperties;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.google.gson.Gson;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.apache.camel.model.dataformat.JsonLibrary;
-import org.apache.camel.model.rest.RestBindingMode;
-import org.springframework.core.env.Environment;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 @Component
 public class PaymentsRoutes extends RouteBuilder {
 
-    private final String KAFKA_TED_INCLUDED = "kafka:%s?brokers=%s";
-    private final String SQS_TED_QUEUE = "aws-sqs://%s?accessKey=%s&secretKey=%s";
     private static final String DYNAMO_DB_CLIENT_ID = "awsDynamoDBClient";
     private final AmazonDynamoDB dynamoDBClient;
     private final KafkaProperties kafkaProperties;
     private final SQSProperties sqsProperties;
-    private final Environment env;
 
-    public PaymentsRoutes(AmazonDynamoDB dynamoDBClient, KafkaProperties kafkaProperties,Environment env,
+    public PaymentsRoutes(AmazonDynamoDB dynamoDBClient, KafkaProperties kafkaProperties,
                           SQSProperties sqsProperties) {
         this.dynamoDBClient = dynamoDBClient;
         this.kafkaProperties = kafkaProperties;
         this.sqsProperties = sqsProperties;
-        this.env = env;
     }
 
     @Override
     public void configure() throws Exception {
         bindToRegistry(DYNAMO_DB_CLIENT_ID, dynamoDBClient);
-
-        //TODO a configuração do rest é só pra facilitar os testes em tempo de desenvolvimento, depois agente apaga esse cara
-        restConfiguration()
-                .contextPath(env.getProperty("camel.component.servlet.mapping.contextPath", "/rest/*"))
-                .apiContextPath("/api-doc")
-                .apiProperty("api.title", "Zup Payments Rest API.")
-                .apiProperty("api.version", "1.0")
-                .apiProperty("cors", "true")
-                .apiContextRouteId("doc-api")
-                .port("8080")
-                .bindingMode(RestBindingMode.json);
-
-        rest("payments")
-                .consumes(MediaType.APPLICATION_JSON_VALUE)
-                .produces(MediaType.APPLICATION_JSON_VALUE)
-                .post("/").route()
-                .marshal().json(JsonLibrary.Gson)
-                .unmarshal().json(JsonLibrary.Gson)
-                .to(String.format(KAFKA_TED_INCLUDED, kafkaProperties.getTedIncluded().getTopicName()))
-                .end();
-
 
         // TODO o payload precisa ser ajustado de acordo com o que vai vir da processor
         from(String.format(kafkaProperties.getKafkaBroker(), kafkaProperties.getTedIncluded().getTopicName()))
@@ -74,14 +44,9 @@ public class PaymentsRoutes extends RouteBuilder {
                     exchange.getIn().setHeader("CamelAwsDdbItem", buildDocument(paymentDTO));
                 })
                 .to("aws-ddb://payments-ted?amazonDDBClient=#" + DYNAMO_DB_CLIENT_ID)
-                .to(String.format(SQS_TED_QUEUE, sqsProperties.getQueue().getTed(), sqsProperties.getAccesskey(), sqsProperties.getSecretkey()))
+                .to(String.format(sqsProperties.getAwsSqs(), sqsProperties.getQueue().getTed()))
         .end();
 
-    }
-
-    private PaymentDTO dtoFromExchangeBody(Exchange exchange) {
-        String payload = exchange.getIn().getBody(String.class);
-        return new Gson().fromJson(payload, PaymentDTO.class);
     }
 
     //TODO isso precisa de um refacory pra ficar mais elegante
